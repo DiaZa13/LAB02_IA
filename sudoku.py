35389c0a6999db8cc287ba74ee3be9d40e64746a
Zaray <cor191025@uvg.edu.gt>
2022-02-13 01:00:03 -0600

    sudoku restrictions from file

--- sudoku.py
+++ sudoku.py
@@ -1,46 +1,27 @@
 from copy import deepcopy
 
-# Para no tener que escribirlo varias veces y que mi linter me siga ayudando
 Sudoku = list[list[int]]
 
-SQUARE_HEIGHT = 3
-SQUARE_LENGTH = 3
-SIZE = SQUARE_LENGTH * SQUARE_HEIGHT
-
-test = [
-    [9, 2, 6, 5, 7, 1, 4, 8, 3],
-    [3, 5, 1, 4, 8, 6, 2, 7, 9],
-    [8, 7, 4, 9, 2, 3, 5, 1, 6],
-    [5, 8, 2, 3, 6, 7, 1, 9, 4],
-    [1, 4, 9, 2, 5, 8, 3, 6, 7],
-    [7, 6, 3, 1, 0, 0, 8, 2, 5],
-    [2, 3, 8, 7, 0, 0, 6, 5, 1],
-    [6, 1, 7, 8, 3, 5, 9, 4, 2],
-    [4, 9, 5, 6, 1, 2, 7, 3, 8],
-]
-'''
-test = [
-    [2, 1, 3, 0, 0, 0],
-    [4, 5, 6, 0, 2, 0],
-    [0, 0, 0, 6, 1, 4],
-    [0, 0, 0, 3, 5, 2],
-    [6, 4, 5, 0, 0, 0],
-    [3, 2, 1, 0, 4, 0]
-]
-'''
-
-
-def find_empty(sudoku: Sudoku) -> [int, int]:
-    for y in range(SIZE):
-        for x in range(SIZE):
+def find_empty(sudoku: Sudoku, size) -> [int, int]:
+    for y in range(size):
+        for x in range(size):
             if sudoku[y][x] == 0:
                 return x, y
 
     return None
 
 
-def find_options(x: int, y: int, sudoku: Sudoku) -> list[int]:
-    return [i for i in range(1, SIZE + 1) if is_valid(sudoku, x, y, i)]
+def find_options(x: int, y: int, sudoku: Sudoku, size) -> list[int]:
+    if size == 6:
+        square_height = 2
+        square_length = 3
+    elif size == 9:
+        square_height = 3
+        square_length = 3
+    else:
+        square_height = 2
+        square_length = 2
+    return [i for i in range(1, size + 1) if is_valid(sudoku, x, y, i, square_height, square_length)]
 
 
 def column(sudoku: Sudoku, n: int) -> list[int]:
@@ -51,55 +32,35 @@ def row(sudoku: Sudoku, n: int) -> list[int]:
     return sudoku[n]
 
 
-def square(sudoku: Sudoku, x: int, y: int) -> list[int]:
-    top = y // SQUARE_HEIGHT * SQUARE_HEIGHT
-    start = x // SQUARE_LENGTH * SQUARE_LENGTH
+def square(sudoku: Sudoku, x: int, y: int, square_height: int, square_length: int) -> list[int]:
+    top = y // square_height * square_height
+    start = x // square_length * square_length
     box = []
 
-    for i in range(SQUARE_HEIGHT):
-        box.extend(sudoku[top + i][start:start + SQUARE_LENGTH])
+    for i in range(square_height):
+        box.extend(sudoku[top + i][start:start + square_length])
 
     return box
 
 
-def is_valid(sudoku: Sudoku, x: int, y: int, n: int) -> bool:
+def is_valid(sudoku: Sudoku, x: int, y: int, n: int, square_height: int, square_length: int) -> bool:
     return not (
             n in row(sudoku, y) or
             n in column(sudoku, x) or
-            n in square(sudoku, x, y)
+            n in square(sudoku, x, y, square_height, square_length)
     )
 
-
-# Creo que esto me tardó más tiempo que hacer el resto del programa xd
-def print_sudoku(sudoku: Sudoku):
-    line_format = ' |'.join([' {}' * SQUARE_LENGTH for _ in range(SQUARE_HEIGHT)]) + '\n'
-    separator = '-' * (2 * (SIZE + SQUARE_LENGTH - 1) + 1) + '\n'
-    result = []
-
-    for i in range(SQUARE_LENGTH):
-        line = ''
-        for j in range(SQUARE_HEIGHT):
-            line += line_format.format(*sudoku[i * SQUARE_HEIGHT + j])
-        result.append(line)
-    print(separator.join(result))
-
-
-def solve(sudoku: Sudoku) -> list[Sudoku]:
-    empty_spot = find_empty(sudoku)
-
+def solve(sudoku: Sudoku, size: int) -> list[Sudoku]:
+    empty_spot = find_empty(sudoku, size)
     if empty_spot is None:
         return [sudoku]
     else:
         x, y = empty_spot
         solutions: list[Sudoku] = []
 
-        for option in find_options(x, y, sudoku):
+        for option in find_options(x, y, sudoku, size):
             copy = deepcopy(sudoku)
             copy[y][x] = option
-            solutions.extend(solve(copy))
+            solutions.extend(solve(copy, size))
         return solutions
 
-
-for solution in solve(test):
-    print_sudoku(solution)
-    print()
--- utilsia.py
+++ utilsia.py
@@ -1,17 +1,61 @@
+from sudoku import solve
 
 Node = int
 Weight = int
 Vertices = list[list[[Node, Weight]]]
 
 def readGraph(file):
-   with open(file, 'r') as graph:
-       node_count = int(graph.readline())
-       edges_count = int(graph.readline())
-       edges: Vertices = [[] for _ in range(node_count)]
-       for edge in graph:
-           x = edge.split()
-           edges[int(x[0])-1].append((int(x[1])-1, int(x[2])))
-   return node_count, edges_count, edges
+    with open(file, 'r') as graph:
+        node_count = int(graph.readline())
+        edges_count = int(graph.readline())
+        edges: Vertices = [[] for _ in range(node_count)]
+        for edge in graph:
+            if edge != '\n':
+                x = edge.split()
+                edges[int(x[0]) - 1].append((int(x[1]) - 1, int(x[2])))
+    return node_count, edges_count, edges
 
-print(readGraph('grafo.txt'))
 
+def readSudoku(file):
+    with open(file, 'r') as sudoku:
+        size = int(sudoku.readline())
+        restrictions_count = int(sudoku.readline())
+        board = [[0 for _ in range(size)] for _ in range(size)]
+        for restriction in sudoku:
+            if restriction != '\n':
+                x = restriction.split()
+                board[int(x[0]) - 1][int(x[1]) - 1] = int(x[2])
+    return size, restrictions_count, board
+
+
+def print_sudoku(sudoku, size, writer):
+    if size == 6:
+        square_height = 2
+        square_length = 3
+    elif size == 9:
+        square_height = 3
+        square_length = 3
+    else:
+        square_height = 2
+        square_length = 2
+
+    line_format = ' |'.join([' {}' * square_length for _ in range(square_height)]) + '\n'
+    separator = '-' * (2 * (size + square_length - 1) + 1) + '\n'
+    result = []
+
+    for i in range(square_length):
+        line = ''
+        for j in range(square_height):
+            line += line_format.format(*sudoku[i * square_height + j])
+        result.append(line)
+    writer.write(separator.join(result))
+
+
+def writeSudoku(file, board, size: int):
+    with open(file, 'w') as sudoku:
+        count = 1
+        for solution in solve(board, size):
+            sudoku.write(f'Solution #{count}\n')
+            print_sudoku(solution, size, sudoku)
+            sudoku.write('\n')
+            count += 1
